4e2513382aeb9c5d1c981acacd57585d4d7d7451
Jennifer Power <barnabei.jennifer@gmail.com>
2026-07-18 15:38:22 -0400

    fix: rename TESTJWKS_TOKEN_DIR to TESTJWKS_OUTPUT_DIR
    Trivy DS-0031 flags any env var containing TOKEN as potential
    secret exposure. Rename to avoid the false positive.
    
    Signed-off-by: Jennifer Power <barnabei.jennifer@gmail.com>

--- cmd/testjwks/main.go
+++ cmd/testjwks/main.go
@@ -30,7 +30,7 @@ func main() {
 		listenAddr = ":8888"
 	}
 
-	tokenDir := os.Getenv("TESTJWKS_TOKEN_DIR")
+	tokenDir := os.Getenv("TESTJWKS_OUTPUT_DIR")
 	if tokenDir == "" {
 		tokenDir = "/tokens"
 	}
